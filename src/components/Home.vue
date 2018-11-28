e9b0fa2a77ff2306f85aefcf14faf9ddc774526e
Lucian Pavel <lucian.pavel@maguay.local>
2018-11-28 17:07:31 +0200

    Changed some stuff. But some things remain the same. Nice.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <v-container>
-        <v-layout row wrap>
+
+        <v-layout row wrap class="mt-2">
             <v-flex xs12 sm6 class="text-sm-right text-xs-center">
                 <v-btn large router to="/meetups" class="info">
                     Explore Meetups
@@ -13,18 +14,28 @@
             </v-flex>
         </v-layout>
         
-        <v-layout row wrap>
+        <v-layout row wrap class="mt-2">
             <v-flex xs12>
                 <v-carousel>
-                    <v-carousel-item v-for="meetup in meetups" v-bind:key="meetup.id" v-bind:src="meetup.imageUrl">
-                        <div class="title">
-                            {{meetup.title}}
-                        </div>
+                    <v-carousel-item v-for="meetup in meetups" v-bind:key="meetup.id">
+                        <v-parallax v-bind:src="meetup.imageUrl">
+                            <div class="title">
+                                {{meetup.title}}
+                            </div>
+                        </v-parallax>
                     </v-carousel-item>
                 </v-carousel>
             </v-flex>
         </v-layout>
+
+        <v-layout row wrap class="mt-2">
+            <v-flex xs12 class="text-xs-center">
+                <p>Join our awesome meetups!</p>
+            </v-flex>
+        </v-layout>
+
     </v-container>
+
 </template>
 
 <script>
@@ -44,8 +55,14 @@
 <style scoped>
     .title{
         position: absolute;
+        width: 50%;
+        left: 25%;
         bottom: 50px;
         background-color: rgba(0, 0, 0, 0.5);
         color: white;
+        padding: 30px;
+        border-top-left-radius: 100%;
+        border-top-right-radius: 100%;
+        text-align: center;
     }
 </style>
